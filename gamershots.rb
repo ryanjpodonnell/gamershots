7d918613788539967676a25e673bee0a27d6ea00
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2015-10-30 13:53:37 -0500

    Add ability to sort by user reviews

--- gamershots.rb
+++ gamershots.rb
@@ -8,6 +8,10 @@ platforms = ""
 publishers = ""
 minimum_year = ""
 maximum_year = ""
+user_reviews = ""
+platforms_query_string = ""
+publishers_query_string = ""
+games = []
 
 get '/' do
   erb :index
@@ -19,32 +23,52 @@ post '/play' do
     publishers = params["publishers"]
     minimum_year = params["minimumYear"]
     maximum_year = params["maximumYear"]
+    user_reviews = params["userReviews"]
+
+    minimum_year = "1935" if minimum_year == "---"
+    maximum_year = "2015" if maximum_year == "---"
+    platforms_query_string = "AND platforms LIKE ANY (VALUES#{platforms.map { |value| "('%#{value}%')" }.join(", ")})" if platforms
+    publishers_query_string = "AND publishers LIKE ANY (VALUES#{publishers.map { |value| "('%#{value}%')" }.join(", ")})" if publishers
+    user_review_query_string = "AND number_of_user_reviews != 0" if user_reviews
   end
 
-  response = connection.exec(<<-SQL)
+  response = connection.exec(<<-SQL) if games.empty?
 SELECT 
-  name, max(image_url) AS image_url 
+  *
 FROM 
   screenshots 
 WHERE
-  publishers LIKE ANY (VALUES#{publishers.map { |value| "('%#{value}%')" }.join(", ")}) 
-AND
-  platforms LIKE ANY (VALUES#{platforms.map { |value| "('%#{value}%')" }.join(", ")}) 
-AND
   original_release_date >= '01-01-#{minimum_year}' 
 AND 
   original_release_date <= '12-31-#{maximum_year}' 
-GROUP BY 
-  name;
+#{platforms_query_string}
+#{publishers_query_string}
+#{user_review_query_string}
   SQL
-  games = response.map { |game| game }
+
+  games = response.map { |game| game } if games.empty?
   @game = games.shuffle!.pop
 
   erb :play
 end
 
 get '/filter' do
-  response = connection.exec("SELECT platforms FROM (SELECT COUNT(*) AS count, platforms FROM screenshots GROUP BY platforms) x WHERE count > 500;") 
+  games = []
+
+  response = connection.exec(<<-SQL)
+SELECT
+  platforms
+FROM 
+  (SELECT
+    COUNT(*) AS count, platforms 
+  FROM
+    screenshots 
+  GROUP BY 
+    platforms) x
+WHERE
+  count > 1000; 
+  SQL
+
   all_platforms = []
   response.each do |result|
     platforms = YAML::load(result["platforms"])
@@ -52,7 +76,20 @@ get '/filter' do
   end
   @all_platforms = all_platforms.sort.uniq
 
-  response = connection.exec("SELECT publishers FROM (SELECT COUNT(*) AS count, publishers FROM screenshots GROUP BY publishers) x WHERE count > 500;") 
+  response = connection.exec(<<-SQL)
+SELECT
+  publishers
+FROM
+  (SELECT
+    COUNT(*) AS count, publishers
+  FROM
+    screenshots
+  GROUP BY
+    publishers) x 
+WHERE
+  count > 1000;
+  SQL
+
   all_publishers = []
   response.each do |result|
     publishers = YAML::load(result["publishers"])
@@ -60,7 +97,15 @@ get '/filter' do
   end
   @all_publishers = all_publishers.sort.uniq
 
-  response = connection.exec("SELECT DISTINCT(EXTRACT(YEAR FROM original_release_date)) AS original_release_year FROM screenshots ORDER BY original_release_year;")
+  response = connection.exec(<<-SQL)
+SELECT
+  DISTINCT(EXTRACT(YEAR FROM original_release_date)) AS original_release_year
+FROM
+  screenshots
+ORDER BY
+  original_release_year;
+  SQL
+
   @years = response.values.flatten
 
   erb :filter
