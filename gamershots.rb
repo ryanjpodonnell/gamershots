207850d0fcbe2662b89eb65318c39818da945a27
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2015-11-19 00:31:26 -0600

    Use cookies.

--- gamershots.rb
+++ gamershots.rb
@@ -2,9 +2,10 @@ require 'pg'
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/cookies'
 require_relative 'screenshot'
 
-db = URI.parse(ENV['DATABASE_URL'])
+#db = URI.parse(ENV['DATABASE_URL'])
 ActiveRecord::Base.establish_connection(
   :adapter  => 'postgresql',
   :host     => db.host,
@@ -14,14 +15,18 @@ ActiveRecord::Base.establish_connection(
   :encoding => 'utf8'
 )
 
-set :params, nil
+enable :sessions
 
 get '/' do
   erb :index
 end
 
 post '/play' do
-  settings.params = params.empty? ? settings.params : params
+  params.each do |key, value|
+    next if ["minimum_year", "maximum_year"].include?(key) && value == "---"
+    session[key] = value
+  end
+
   @screenshot = _filter_screenshots
   return erb :index if @screenshot.nil?
 
@@ -29,13 +34,10 @@ post '/play' do
 end
 
 get '/filter' do
-  settings.params = nil
-
   erb :filter
 end
 
 def _filter_screenshots
-  _delete_unused_year_params
   methods = _build_method_chain
 
   methods.inject(Screenshot) { |obj, method| obj.send(*method) }
@@ -43,31 +45,25 @@ def _filter_screenshots
     .first
 end
 
-def _delete_unused_year_params
-  ["minimum_year", "maximum_year"].each do |field|
-    settings.params.delete(field) if settings.params[field] == "---"
-  end
-end
-
 def _build_method_chain
-  settings.params.keys.map do |field|
+  session.keys.map do |field|
     case field
     when "platforms", "publishers"
       _build_like_method(field)
     when "minimum_year"
-      minimum_date = "01-01-#{settings.params[field]}"
+      minimum_date = "01-01-#{session[field]}"
       _build_equality_method(">=", "original_release_date", minimum_date)
     when "maximum_year"
-      maximum_date = "12-31-#{settings.params[field]}"
+      maximum_date = "12-31-#{session[field]}"
       _build_equality_method("<=", "original_release_date", maximum_date)
     when "number_of_user_reviews"
       _build_equality_method(">=", field, "1")
     end
-  end
+  end.compact
 end
 
 def _build_like_method(field)
-  selected_values = settings.params[field].map{ |value| "%#{value}%" }
+  selected_values = session[field].map{ |value| "%#{value}%" }
   query = (["#{field} LIKE ?"] * selected_values.count).join(" OR ")
   [:where, query, *selected_values]
 end
